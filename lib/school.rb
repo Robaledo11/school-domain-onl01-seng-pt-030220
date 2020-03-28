f8f93991cdbc7c0571e9a4cfdc964f5f20262475
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-03-28 19:16:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,8 +7,8 @@ class School
   end
 
 def add_student(name,grade)
-  roster[grade] = [] unless roster
-  roster[grade << name 
+  roster[grade] = [] unless roster[grade]
+  roster[grade] << name 
 end
 end
    
