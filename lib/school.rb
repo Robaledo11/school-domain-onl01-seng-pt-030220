83eb649a1356f708c1dbdcbdc82d421c054a9244
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-03-28 19:04:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,15 +3,6 @@ class Student
 def initialize(name)
   @name=name 
   @roster= {}
-end
-   
-   def roster
-     @roster
-   end
-   
-   def add_students (name,grade)
-     grade[roster] = {}
-     grade[rosrwe] = name 
-    end
+
    
    
